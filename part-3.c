97fed2601e970a6cf71b91e5ea35adf7db3dd3a3
shuyi0220 <93233711+shuyi0220@users.noreply.github.com>
2022-03-01 23:24:37 -0800

    Complete part3 main

--- part-3.c
+++ part-3.c
@@ -1,6 +1,6 @@
 /*
  * file:        part-3.c
- * description: part 3, CS5600 Project 1, Spring 2021
+ * description: part 3, CS5600 Project 1, Spring 2022
  */
 
 /* NO OTHER INCLUDE FILES */
@@ -25,16 +25,28 @@
 #define FUNCTION_SUCCESS 0
 #define FUNCTION_FAILURE -1
 #define M_OFFSET    0x1000000
+#define M2_OFFSET    0x2000000
 
-/* Global  variables for do_getarg */
-char *argv[10];
-int argc;
+/* Type definitions */
 
+typedef void (*pFunc)();
 
-typedef struct {    
-    void *addr;
-    int len;
-} memory_t;
+/* Global variables */
+
+char *argv[2] = {"process1", "process2"};
+
+int argc = 2;
+
+char stack1[4096];    /* Process 1 stack */
+
+char stack2[4096];    /* Process 2 stack */
+
+void *pMainStack;    /* Main process stack pointer */
+
+void *pStack1;    /* Process 1 stack pointer */
+
+void *pStack2;    /* Process 2 stack pointer */
+
 
 extern void *vector[];
 extern void switch_to(void **location_for_old_sp, void *new_value);
@@ -177,10 +189,52 @@ char *do_getarg(int i){
 	return argv[i];
 }
 
+void load_program(int fd, int offset)
+{
+		/* read the main header (offset 0) */
+		struct elf64_ehdr hdr;
+		lseek(fd, 0, SEEK_SET);
+		read(fd, &hdr, sizeof(hdr));
+		int n = hdr.e_phnum;
 
+		/* read program headers (offset 'hdr.e_phoff') */
+		struct elf64_phdr phdrs[n];
+		lseek(fd, hdr.e_phoff, SEEK_SET);
+		read(fd, phdrs, sizeof(phdrs));
 
-/* copy from Part 2 */
-void do_print(char *buf);
+		void *start_addr = (void *)0x0 + offset;
+		int len[n];
+
+		int i;
+		for (i = 0; i < n; i++) {
+				if (phdrs[i].p_type == PT_LOAD) {
+						len[i] = ROUND_UP(phdrs[i].p_memsz, 4096);
+
+						void *buf = mmap(start_addr, len[i], PROT_READ | PROT_WRITE | PROT_EXEC, 
+								 MAP_PRIVATE | MAP_ANONYMOUS, fd, 0);
+						if (buf == MAP_FAILED) {
+								do_print("mmap failed\n");
+								exit(EXIT_FAILURE);
+						}
+
+						start_addr += len[i];
+
+						lseek(fd, (int)phdrs[i].p_offset, SEEK_SET);
+						read(fd, buf, phdrs[i].p_memsz);
+				}
+		}
+}
+
+pFunc get_entry_point(int fd, int offset)
+{
+		/* Get entry address for mapped address allocation. */
+		struct elf64_ehdr hdr;
+		lseek(fd, 0, SEEK_SET);
+		read(fd, &hdr, sizeof(hdr));
+
+		pFunc fptr = (pFunc)(hdr.e_entry + offset);
+		return fptr;
+}
 
 /* ---------- */
 
@@ -233,6 +287,35 @@ void main(void)
 
 	/* your code here */
 
+	int exit_code = EXIT_FAILURE;
+  	int index = 0;
+  	int fd1, fd2;
+  	pFunc fptr1, fptr2;
+
+ 	 char *arg = do_getarg(index);
+	fd1 = open(arg, O_RDONLY);
+	
+	index = 1;
+  	arg = do_getarg(index);
+	fd2 = open(arg, O_RDONLY);
+
+
+	load_program(fd1, M1_OFFSET);
+	load_program(fd2, M2_OFFSET);
+	fptr1 = get_entry_point(fd1, M1_OFFSET);
+	fptr2 = get_entry_point(fd2, M2_OFFSET);
+
+	pStack1 = setup_stack0(stack1 + 4096, fptr1);
+	pStack2 = setup_stack0(stack2 + 4096, fptr2);
+
+	switch_to(&pMainStack, pStack1);
+
 	do_print("done\n");
-	exit(0);
+
+	close(fd1);
+	close(fd2);
+
+	exit_code = EXIT_SUCCESS;
+
+	exit(exit_code);
 }
